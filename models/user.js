d9aa0d46f7c8dcb0435c368fc63dac77561abba1
race-briggs <race.briggs@gmail.com>
2019-10-18 11:44:36 -0500

    added User model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("Post", {
+    name: DataTypes.STRING
+  });
+
+  User.associate = function(models){
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+};
