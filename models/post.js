62e1729c6ec0f715e3817e4ccf93c7ffab1df5c4
race-briggs <race.briggs@gmail.com>
2019-10-18 13:03:46 -0500

    changed endpoint names

--- models/post.js
+++ models/post.js
@@ -1,9 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
-  var Post = sequelize.define("Example", {
+  var Post = sequelize.define("Post", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  Post.associate = function(models){
+  Post.associate = function(models) {
     Post.belongsTo(models.User, {
       foreignKey: {
         allowNull: false
